dacb212f69b61a144afd5d9232a23e7c58ddc49f
Sophia <snkcacademia@gmail.com>
2024-07-11 16:14:32 -0400

    whatttttt pls push :(

---
+++ numpy_practice.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# p1
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(f"p1 answer: {a+b} and {a-b}")
+
+# p2
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+print(a)
+print(f"p2 answer: {a+b} and {a-b}")
+
+# p3
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(f"p3 answer: {np.dot(a,b)}")
