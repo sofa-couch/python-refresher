da50b014a713f44e086e715c5be453e415966b3f
Sophia <snkcacademia@gmail.com>
2024-07-12 07:53:34 -0400

    finished linalg stuff

--- numpy_practice.py
+++ numpy_practice.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 # p1
 a = np.array([1, 2, 3])
@@ -15,3 +16,16 @@ print(f"p2 answer: {a+b} and {a-b}")
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 print(f"p3 answer: {np.dot(a,b)}")
+
+# p4
+a = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[7, 8, 9, 10], [11, 12, 13, 14], [15, 16, 17, 18]])
+print(f"p4 answer: {np.dot(a, b)}")
+
+# p5
+a = np.array([1, 2, 3])
+print(f"p5 answer: {np.linalg.norm(a)}")
+
+# p6
+a = np.array([[1, 2], [3, 4]])
+print(f"p6 answer: {a.T}")
